eb82089c114f0f1ff05e1b39f72bfe271aa4219f
Kolsky <22716073+Kolsky@users.noreply.github.com>
2024-12-03 02:13:44 +0500

    compress compress compress

--- src/bin/d2.rs
+++ src/bin/d2.rs
@@ -6,17 +6,15 @@ fn main() -> eyre::Result<()> {
             .map(|num| num.parse::<u32>())
             .collect::<Result<Vec<_>, _>>()?;
         let crit = |f: fn(_, _) -> bool| {
-            let Some(p) = nums.windows(2).position(|w| !f(w[0], w[1])) else {
-                return 2;
-            };
-            (nums[p + 2..].windows(2).all(|w| f(w[0], w[1]))
-                && ((p == 0 || f(nums[p - 1], nums[p + 1]))
-                    && (p == nums.len() - 2 || f(nums[p + 1], nums[p + 2]))
-                    || (p == nums.len() - 2 || f(nums[p], nums[p + 2])))) as u32
+            let p = nums.windows(2).position(|w| !f(w[0], w[1]))?;
+            let q = nums.windows(2).rposition(|w| !f(w[0], w[1]))?;
+            let b = p == q && (p == 0 || f(nums[p - 1], nums[p + 1]))
+                || p + 1 >= q && (p + 2 == nums.len() || f(nums[p], nums[p + 2]));
+            Some(b)
         };
-        let k = crit(|x, y| x < y && y - x <= 3).max(crit(|y, x| x < y && y - x <= 3));
-        ans1 += u32::from(k > 1);
-        ans2 += k.min(1);
+        let k = crit(|x, y| x < y && y - x <= 3).zip(crit(|y, x| x < y && y - x <= 3));
+        ans1 += k.map_or(1, |_| 0);
+        ans2 += k.map_or(1, |(x, y)| u32::from(x | y));
     }
 
     println!("{ans1}\n{ans2}");
