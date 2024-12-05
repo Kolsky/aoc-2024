70ec7a626b65071d14ddf40ccb9329524cd77b09
Kolsky <22716073+Kolsky@users.noreply.github.com>
2024-12-05 05:30:44 +0500

    faster

--- src/bin/d4o.rs
+++ src/bin/d4o.rs
@@ -1,15 +1,32 @@
+#![feature(portable_simd)]
+
+use std::simd::*;
+use cmp::SimdPartialEq;
+
+fn bits(t: &[u8; 141], c: &[u8; 141], b: &[u8; 141], offset: usize) -> u32 {
+    let w = u8x16::from_slice(&t[offset..]);
+    let x = u8x16::from_slice(&t[offset + 2..]);
+    let a = u8x16::from_slice(&c[offset + 1..]);
+    let y = u8x16::from_slice(&b[offset..]);
+    let z = u8x16::from_slice(&b[offset + 2..]);
+
+    let wz = (w ^ z).simd_eq(u8x16::splat(b'M' ^ b'S'));
+    let xy = (x ^ y).simd_eq(u8x16::splat(b'M' ^ b'S'));
+    let a = a.simd_eq(u8x16::splat(b'A'));
+
+    (wz & a & xy).to_bitmask() as u32
+}
+
 pub fn run(input: &[u8]) -> u32 {
     let input = unsafe { input.align_to::<[u8; 141]>() }.1;
     assert_eq!(input.len(), 140);
     let mut sum = 0;
     for lanes in input.windows(3) {
-        let (t, c, b) = (lanes[0], lanes[1], lanes[2]);
-        for i in 1..139 {
-            let (w, x, y, z) = (t[i - 1], t[i + 1], b[i - 1], b[i + 1]);
-            if c[i] == b'A' && (w ^ x ^ y ^ z) == 0 && (x ^ y) != 0 {
-                sum += u32::from((x ^ y) == (b'M' ^ b'S'));
-            }
+        let (t, c, b) = (&lanes[0], &lanes[1], &lanes[2]);
+        for i in 0..8 {
+            sum += bits(t, c, b, 16 * i).count_ones();
         }
+        sum += (bits(t, c, b, 16 * 8 - 6) & (!63u32)).count_ones();
     }
     sum
 }
